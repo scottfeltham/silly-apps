6873010f5ee315b21caf3c2c25c628506cfc2744
Scott Feltham <scott@outdoorsphere.co.uk>
2025-10-21 19:19:21 +0400

    Fixed viewports

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fidget-fun-v2';
+const CACHE_NAME = 'fidget-fun-v3';
 const BASE_PATH = '/silly-apps'; // Change this if your repo name is different
 const urlsToCache = [
   `${BASE_PATH}/`,
